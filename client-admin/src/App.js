c78e9b39aa96e872e3669ebdb40d618012745b6f
aplchian <alex.boquis@gmail.com>
2017-08-26 17:51:21 -0400

    auth working

--- client-admin/src/App.js
+++ client-admin/src/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react"
 import "./App.css"
 import NavBar from "./components/NavBar"
-import EventsList from './pages/Events/list'
+import EventsList from "./pages/Events/list"
+import Auth from "./auth/Auth.js"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import Callback from './auth/Callback'
+const auth = new Auth()
+const { isAuthenticated, logout, login } = auth
+
+const handleAuthentication = (nextState, replace) => {
+  if (/access_token|id_token|error/.test(nextState.location.hash)) {
+    auth.handleAuthentication()
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App pb5">
-        <NavBar />
-        <EventsList />
-      </div>
+      <Router>
+        <div className="App pb5">
+          {isAuthenticated()
+            ? <button onClick={logout}>Logout!</button>
+            : <button onClick={login}>Login</button>}
+          <NavBar />
+          <Route exact path="/" component={EventsList} />
+          <Route
+            path="/callback"
+            render={props => {
+              handleAuthentication(props)
+              return <Callback {...props} />
+            }}
+          />
+        </div>
+      </Router>
     )
   }
 }
