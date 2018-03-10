78a2006a51a4f69f92d7889d9e1e666af766d752
aplchian <alex.boquis@gmail.com>
2018-03-10 18:17:47 -0500

    updated input height

--- client-admin/src/components/MaterialInput/index.js
+++ client-admin/src/components/MaterialInput/index.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import React from "react"
 import Input from "material-ui/Input"
 import InputLabel from "material-ui/Input/InputLabel"
 import FormControl from "material-ui/Form/FormControl"
 
-
 export default props => {
   const multiline = props.type === "textarea"
+  const inputProps = !multiline
+    ? {
+        style: {
+          height: "30px"
+        }
+      }
+    : {}
+
   return (
     <div className="mb2">
       <FormControl fullWidth className={props.classes.formControl}>
         <div className="w-90">
-          <InputLabel htmlFor="name">
-            {props.placeholder}
-          </InputLabel>
+          <InputLabel htmlFor="name">{props.placeholder}</InputLabel>
           <Input
             fullWidth
             multiline={multiline}
@@ -20,11 +25,7 @@ export default props => {
             value={props.input.value}
             {...props.input}
             rows={5}
-            inputProps={{
-              style: {
-                height: '30px'
-              }
-            }}
+            inputProps={inputProps}
           />
         </div>
       </FormControl>
